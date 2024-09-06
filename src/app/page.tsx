4760f582da2c39706623b52ffe8a17ba17aad380
FabricioOlivera <oliverafabricio25@gmail.com>
2024-09-06 19:53:49 -0300

    chore: redirect from root to register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = "/register";
+  }, []);
   return <div className={styles.page}></div>;
 }
